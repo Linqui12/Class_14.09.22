3864460a1989c5c559e05b258f4b60d0dda45356
Linqui12 <100015682+Linqui12@users.noreply.github.com>
2022-09-14 18:55:43 +0300

    git init

---
+++ class.js
@@ -0,0 +1,27 @@
+'use strict'
+
+//создать пользователя бимя фамилия,мыло,возраст,статус логина
+
+
+// const userProto={firstName,lastName,age
+//   firstName= firstname,
+//   lastNane= lastName,
+//   age= age,
+//   onlineStatus=false,
+//   isLogin : function(){
+//     return this.onlineStatus;
+//   },
+//   logIn : function(){
+//     this.onlineStatus =true;
+//     return "Welcome";
+//   },
+//   logOut:function(){
+//     this.onlineStatus=false;
+//     return "Bye";
+//   }
+// }
+// User1.prototype =userProto;
+
+//globalObject//////////////
+const str1 ='tastes';
+const str2 =str1 +' oy';
